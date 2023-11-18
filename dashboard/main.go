deace8d98c68b631c84b6e9657c1c0caeaa01afb
vipul.gupta <vipulgupta42all@gmail.com>
2023-11-18 21:58:02 +0530

    added env support for db data

--- dashboard/main.go
+++ dashboard/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 
 	"github.com/priyanshu360/lab-rank/dashboard/api"
 	"github.com/priyanshu360/lab-rank/dashboard/config"
 	"github.com/priyanshu360/lab-rank/dashboard/utils"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error in loading env file, Generate .env file")
+		return
+	}
 	loggerConf := config.InitLoggerConfig()
 	logger := utils.NewLogger(loggerConf)
 	slog.SetDefault(logger)
