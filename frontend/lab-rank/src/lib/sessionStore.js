180f9b3dabeaf018a44ed4da36abb7f1e0df5b6f
priyanshu360 <priyanshurajput360@gmail.com>
2024-01-31 11:11:30 +0530

    removed frontend

--- frontend/lab-rank/src/lib/sessionStore.js
+++
@@ -1,7 +0,0 @@
-// sessionStore.js
-import { writable } from 'svelte/store';
-
-export const sessionStore = writable({
-  subjects: [], // Initial subjects array
-  // Add other properties as needed
-});
--- frontend/lab-rank/src/routes/logout/+page.server.js
+++
@@ -1,6 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const load = ({ cookies }) => {
-    cookies.delete("jwt_lab_rank")
-    throw redirect(303, '/login')
-}
